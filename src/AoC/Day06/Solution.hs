7167efe0ab3826280d0ba903054518134c2fd7ab
Mirko Westermeier <mirko@westermeier.de>
2022-12-13 11:56:13 +0100

    Finish 6

--- src/AoC/Day06/Solution.hs
+++ src/AoC/Day06/Solution.hs
@@ -1,7 +1,17 @@
 module AoC.Day06.Solution where
 
-solve06_1 :: String -> String
-solve06_1 = undefined
+import Data.List
+import Data.Maybe
 
-solve06_2 :: String -> String
-solve06_2 = undefined
+firstNdifferent :: Eq a => Int -> [a] -> Maybe Int
+firstNdifferent n xs
+  | length xs < n     = Nothing
+  | uniq (take n xs)  = Just n
+  | otherwise         = succ <$> firstNdifferent n (tail xs)
+  where uniq = (==) <*> nub
+
+solve06_1 :: String -> Int
+solve06_1 = fromJust . firstNdifferent 4
+
+solve06_2 :: String -> Int
+solve06_2 = fromJust . firstNdifferent 14
